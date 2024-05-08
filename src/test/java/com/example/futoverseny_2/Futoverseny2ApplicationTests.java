b759a20dc484b19085f798c48b4cb6d65caec75f
MeszesBalint <meszesbalint@gmail.com>
2024-05-08 13:32:32 +0200

    elso

---
+++ src/test/java/com/example/futoverseny_2/Futoverseny2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.futoverseny_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Futoverseny2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
